345afe05661ddd82180a017be412aee500c635ab
sofyan <sofyansyah.ss@gmail.com>
2017-05-17 01:11:42 +0700

    'sad'

--- resources/views/emotion/index.blade.php
+++ resources/views/emotion/index.blade.php
@@ -19,12 +19,12 @@
 	}
 	.emotdate{
 		text-align: center;
-	font-size: 10px; 
-	padding: 5px; 
-	background-color: #f1c40f; 
-	margin-top: 10px; 
-	margin-right: -18px;
-	border-radius: 5px;"
+		font-size: 10px; 
+		padding: 5px; 
+		background-color: #f1c40f; 
+		margin-top: 10px; 
+		margin-right: -18px;
+		border-radius: 5px;"
 	}
 </style>
 
@@ -33,36 +33,40 @@
 	
 	<div class="col-md-12 nopadding">
 		<textarea style="width: 100%; height:100px; border:none;"></textarea>
-		@forelse( $emotions as $emotion )
+		@forelse( $emotions as $feels )
 		{{-- @if ($emotion->user_id ==Auth::id()) --}}
 
 		<section id="cd-timeline" class="cd-container">
 			<div class="cd-timeline-block">
 				<div class="cd-timeline-img cd-movie">
-					<img src="{{asset('img/emot/'.$emotion->emot)}}" class="img-circle" height="100px" width:100px; alt="">
-					<p class="emotdate"> {{$emotion->created_at->diffForHumans()}}</p>
+					<img src="{{asset('img/emot/'.$feels->emot)}}" class="img-circle" height="100px" width="100px;" alt="">
+					<p class="emotdate"> {{$feels->created_at->diffForHumans()}}</p>
 				</div> <!-- cd-timeline-img -->
 				<div class="cd-timeline-content">
-					<p><a href={{url('emotion/'.$emotion->id)}}>
-						{{-- $user->avatar --}}{{-- $user->username --}}
-						<h4 style="padding:0px; margin:0px;">{{$emotion->text}}</h4></a></p>
-						
-					</div> <!-- cd-timeline-content -->
-				</div> <!-- cd-timeline-block -->
+					<a href={{url('emotion/'.$feels->id)}}>
+						<div class="col-md-2">
+							<img src="{{asset('img/avatar/'.$feels->avatar)}}" class="img-circle" height="40px" width="40px;" alt=""></div>
+							<div class="col-md-10">
+								<h4>@ {{ $feels->username }}</h4>
+							</div>
+							<div class="col-md-12">
+								<p>{{$feels->text}}</p></div></a>
+							</div> <!-- cd-timeline-content -->
+						</div> <!-- cd-timeline-block -->
 
 
-			</section> <!-- cd-timeline -->
+					</section> <!-- cd-timeline -->
 
-			{{-- @endif --}}
-			@empty
-			No Emot
+					{{-- @endif --}}
+					@empty
+					No Emot
 
-			@endforelse
+					@endforelse
 
-		</div>
+				</div>
 
 
-	</div>
+			</div>
 
-	@endsection
+			@endsection
 
